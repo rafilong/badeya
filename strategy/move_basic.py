9f9e88a70b146bce337c4fdb03da20bcb1b37e58
Raphael Long <rafilong42@gmail.com>
2018-09-22 05:27:43 -0500

    Realized that randint is inclusive on both bounds

--- strategy/move_basic.py
+++ strategy/move_basic.py
@@ -7,8 +7,7 @@ class MoveBasic(MoveStrategy):
     def select_move(self):
         if not self.moving() and not self.has_alive_monster():
             monsters = self.game.nearest_monsters(self.next_location(), 1)
-            # seems like monsters length can be 0? shouldn't be possible but next line is throwing array out of bounds
-            paths = self.game.shortest_paths(self.me.location, monsters[random.randint(0, len(monsters))].location)
+            paths = self.game.shortest_paths(self.me.location, monsters[random.randint(0, len(monsters) - 1)].location)
             if len(paths) != 0:
                 return paths[0][0]
         return self.me.destination
