8609874f0618c9f83bab1308419dce770cb348b0
Raphael Long <rafilong42@gmail.com>
2018-09-22 04:28:50 -0500

    Fix config.py imports

--- strategy/Config.py
+++
@@ -1,7 +0,0 @@
-from strategy.move_basic import MoveRandom
-from strategy.stance_basic import StanceRandom
-
-STRATEGY = {
-    "move": MoveRandom,
-    "stance": StanceRandom
-}
---
+++ strategy/config.py
@@ -0,0 +1,7 @@
+from strategy.move_random import MoveRandom
+from strategy.stance_random import StanceRandom
+
+STRATEGY = {
+    "move": MoveRandom,
+    "stance": StanceRandom
+}
