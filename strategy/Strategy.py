a449f089729e382b2e27e29e57b66cdbbc865935
Raphael Long <rafilong42@gmail.com>
2018-09-22 04:58:39 -0500

    Add more strategy utility functions

--- strategy/Strategy.py
+++ strategy/Strategy.py
@@ -13,6 +13,18 @@ class Strategy(ABC):
     def update(self):
         pass
 
+    def will_move(self):
+        if (self.me.movement_counter + 1 == self.me.speed):
+            return True
+        else:
+            return False
+
+    def next_location(self):
+        if self.will_move():
+            return self.me.destination
+        else:
+            return self.me.location
+
 class MoveStrategy(Strategy):
 
     def get_move(self):
