ead7f66412caca8f16e987308c530aac0ba1523b
Raphael Long <rafilong42@gmail.com>
2018-09-22 05:22:59 -0500

    Fix stance_basic to actually counter stance

--- strategy/stance_basic.py
+++ strategy/stance_basic.py
@@ -6,6 +6,6 @@ class StanceBasic(StanceStrategy):
 
     def select_stance(self):
         if self.game.has_monster(self.next_location):
-            return self.get_winning_stance(self.game.get_monster(self.next_location))
+            return self.get_winning_stance(self.game.get_monster(self.next_location).stance)
         else:
             return StanceStrategy.STANCES[random.randint(0, 2)]
