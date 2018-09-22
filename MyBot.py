22865bbed05f9432615fbcece86d2d83c59114f2
Raphael Long <rafilong42@gmail.com>
2018-09-22 01:43:33 -0500

    Modify MyBot to use strategy classes

--- MyBot.py
+++ MyBot.py
@@ -8,6 +8,9 @@ import random
 
 first_line = True # DO NOT REMOVE
 
+move_strategy = None;
+stance_strategy = None;
+
 # global variables or other functions can go here
 STANCES = ["Rock", "Paper", "Scissors"]
 
@@ -31,27 +34,8 @@ for line in fileinput.input():
 
     # code in this block will be executed each turn of the game
 
-    me = game.get_self()
-
-    if me.location == me.destination: # check if we have moved this turn
-        # get all living monsters closest to me
-        monsters = game.nearest_monsters(me.location, 1)
-
-        # choose a monster to move to at random
-        monster_to_move_to = monsters[random.randint(0, len(monsters)-1)]
-
-        # get the set of shortest paths to that monster
-        paths = game.shortest_paths(me.location, monster_to_move_to.location)
-        destination_node = paths[random.randint(0, len(paths)-1)][0]
-    else:
-        destination_node = me.destination
-
-    if game.has_monster(me.location):
-        # if there's a monster at my location, choose the stance that damages that monster
-        chosen_stance = get_winning_stance(game.get_monster(me.location).stance)
-    else:
-        # otherwise, pick a random stance
-        chosen_stance = STANCES[random.randint(0, 2)]
+    destination = move_strategy.whichMove()
+    stance = stance_strategy.whichStance()
 
     # submit your decision for the turn (This function should be called exactly once per turn)
     game.submit_decision(destination_node, chosen_stance)
