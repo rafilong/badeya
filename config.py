5184728baab746f5ce7967593eec2a7c3860aeaa
Raphael Long <rafilong42@gmail.com>
2018-09-22 05:18:11 -0500

    Use basic strategies

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from strategy import *
 
 STRATEGY = {
-    "move": move_random.MoveRandom,
-    "stance": stance_random.StanceRandom
+    "move": move_basic.MoveBasic,
+    "stance": stance_basic.StanceBasic
 }
